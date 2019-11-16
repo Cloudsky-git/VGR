503796290b876c36889944006b03d83d3764593f
Andrzej <maniuuur@gmail.com>
2019-11-16 22:41:44 +0100

    Register

--- Video Games Rental/Video Games Rental/Controllers/HomeController.cs
+++ Video Games Rental/Video Games Rental/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace video_games_rental.Controllers
 {
     public class HomeController : Controller
---
+++ Video Games Rental/Video Games Rental/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Video_Games_Rental.Models
+{
+    public class IdentityModels
+    {
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        //You can extend this class by adding additional fields like Birthday
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+    }
+}
