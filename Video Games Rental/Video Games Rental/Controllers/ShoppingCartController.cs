2201dd1df41bc7b4061cec6a0d71bf8b31cf952d
Andrzej <maniuuur@gmail.com>
2019-11-26 03:18:05 +0100

    shopping cart - not ready visually yet tho

---
+++ Video Games Rental/Video Games Rental/Controllers/ShoppingCartController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Video_Games_Rental.Models;
+
+namespace Video_Games_Rental.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private VGR_DBEntities db = new VGR_DBEntities();
+        private string strCart = "Cart";
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderNow(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            if (Session[strCart] == null)
+            {
+                List<Cart> lsCart = new List<Cart>
+                {
+                    new Cart(db.games.Find(id),1)
+                };
+
+                Session[strCart] = lsCart;
+            }
+
+            else
+            {
+                List<Cart> lsCart = (List<Cart>)Session[strCart];
+                int check = isExistingCheck(id);
+                if (check == -1)
+                    lsCart.Add(new Cart(db.games.Find(id), 1));
+                else
+                    lsCart[check].Quantity++;
+                Session[strCart] = lsCart;
+            }
+            return View("Index");
+
+        }
+
+        private int isExistingCheck(int? id)
+        {
+            List<Cart> lsCart = (List<Cart>)Session[strCart];
+            for (int i=0; i<lsCart.Count; i++)
+            {
+                if (lsCart[i].Product.game_id == id) return i;
+            }
+            return -1;
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            int check = isExistingCheck(id);
+            List<Cart> lsCart = (List<Cart>)Session[strCart];
+            lsCart.RemoveAt(check);
+            return View("Index");
+        }
+    }
+}
---
+++ Video Games Rental/Video Games Rental/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Video_Games_Rental.Models
+{
+    public class Cart
+    {
+
+        public game Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Cart(game product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
