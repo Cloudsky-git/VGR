429873886472286d063c341afc9699142bd18b12
Andrzej <maniuuur@gmail.com>
2019-12-31 01:30:07 +0100

    filtering

--- Video Games Rental/Video Games Rental/Controllers/ShopController.cs
+++ Video Games Rental/Video Games Rental/Controllers/ShopController.cs
@@ -17,14 +17,69 @@ namespace Video_Games_Rental.Controllers
             return View();
         }
 
-        public PartialViewResult GameListPartial(int? page)
+        public PartialViewResult GameListPartial(int? page,int? platform, int? language, int? condition)
         {           
             var pageNumber = page ?? 1;
             var pageSize = 9;
-            var gameList = db.games.OrderByDescending(x => x.game_id).ToPagedList(pageNumber, pageSize);
-            return PartialView(gameList);                    
+           
+            if (platform != null)
+            {
+                ViewBag.platform = platform;
+                var gameList = db.games
+                    .OrderByDescending(x => x.game_id)
+                    .Where(x=> x.platform_id == platform)
+                    .ToPagedList(pageNumber, pageSize);
+                    
+                return PartialView(gameList);
+            }
+
+            else if (language != null)
+            {
+                ViewBag.language = platform;
+                var gameList = db.games
+                    .OrderByDescending(x => x.game_id)
+                    .Where(x => x.language_id == language)
+                    .ToPagedList(pageNumber, pageSize);
+
+                return PartialView(gameList);
+            }
+
+            else if (condition != null)
+            {
+                ViewBag.condition = platform;
+                var gameList = db.games
+                    .OrderByDescending(x => x.game_id)
+                    .Where(x => x.condition_id == condition)
+                    .ToPagedList(pageNumber, pageSize);
+
+                return PartialView(gameList);
+            }
+
+            else
+            {
+                var gameList = db.games.OrderByDescending(x => x.game_id).ToPagedList(pageNumber, pageSize);
+                return PartialView(gameList);
+            }   
         }
 
+        public PartialViewResult PlatformListPartial()
+        {
+            var platformList = db.platforms.OrderByDescending(x => x.platform1).ToList();
+            return PartialView(platformList);
+        }
+
+        public PartialViewResult LanguageListPartial()
+        {
+            var languageList = db.languages.OrderByDescending(x => x.language1).ToList();
+            return PartialView(languageList);
+        }
+
+
+        public PartialViewResult ConditionListPartial()
+        {
+            var languageList = db.conditions.OrderByDescending(x => x.condition1).ToList();
+            return PartialView(languageList);
+        }
         public ActionResult Details(int? id)
         {
             if(id == null)
